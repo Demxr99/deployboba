c19a8b7cd520d459ec10ce348f85d15aa5a5e4eb
Demar Edwards <dedwards@mit.edu>
2019-01-17 18:06:07 -0500

    Added passport.js and fixed links to css for server

---
+++ src/passport.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const User = require('./models/user');
+
+// set up passport configs
+passport.use(new GoogleStrategy({
+  clientID: '751838639139-b1rvgmp2jb8aie3oenicj8lusaa4e23q.apps.googleusercontent.com',
+  clientSecret: 'Xy7qQbCi5kUTXs4B2p1A3ZEK',
+  callbackURL: '/auth/google/callback'
+}, function(accessToken, refreshToken, profile, done) {
+  User.findOne({
+    'googleid': profile.id
+  }, function(err, user) {
+    if (err) return done(err);
+
+    if (!user) {
+      const user = new User({
+        name: profile.displayName,
+        googleid: profile.id
+      });
+
+      user.save(function(err) {
+        if (err) console.log(err);
+
+        return done(err, user);
+      });
+    } else {
+      return done(err, user);
+    }
+  });
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+module.exports = passport;
