8cb1429130eee22bebd50f71bc726b29f9b3af98
Lucy Wang <lucywang@mit.edu>
2019-01-17 17:46:35 -0500

    Navbar items added through navbar.js

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,6 +1,6 @@
 function newNavbarItem(text, url) {
   const itemLink = document.createElement('a');
-  itemLink.className = 'item';
+  itemLink.className = 'ui item';
   itemLink.innerHTML = text;
   itemLink.href = url;
 
@@ -8,14 +8,44 @@ function newNavbarItem(text, url) {
 }
 
 function renderNavbar(user) {
-  const navbarDiv = document.getElementsByClassName("ui stackable inverted teal menu");
+  const navbarDiv = document.getElementById("navbar");
 
-  navbarDiv.appendChild(newNavbarItem('Home', '/'));
+  navbarDiv.appendChild(newNavbarItem('About', '/about-us'));
+  navbarDiv.appendChild(newNavbarItem('Maps', '/map'));
 
-  if (user._id !== undefined) {
+  if (typeof(user) !== 'undefined' && user._id !== undefined) {
     navbarDiv.appendChild(newNavbarItem('Profile', '/u/profile?'+user._id));
-    navbarDiv.appendChild(newNavbarItem('Logout', '/logout'));
-  } else {
-    navbarDiv.appendChild(newNavbarItem('Login', '/auth/google'));
+    navbarDiv.appendChild(newNavbarItem('Add Store/Event', '/add-store-event'));
   }
+  navbarDiv.appendChild(navbarRightMenu());
+}
+
+function navbarRightMenu() {
+  const rightMenu = document.createElement('div');
+  rightMenu.className = "right menu";
+
+  //create search bar in navbar
+  const search = document.createElement('div');
+  search.className = 'item';
+  
+  const searchBar = document.createElement('div');
+  searchBar.className = 'ui icon input';
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.placeholder = 'Search...'
+  const i = document.createElement('i');
+  i.className = 'search link icon'
+  searchBar.appendChild(input);
+  searchBar.appendChild(i);
+  search.append(searchBar);
+  rightMenu.appendChild(search);
+
+  //login or logout depending on whether the user logged in
+  if (typeof(user) !== 'undefined' && user._id !== undefined) {
+    rightMenu.appendChild(newNavbarItem('Logout', '/logout'));
+  } else {
+    rightMenu.appendChild(newNavbarItem('Login', '/auth/google'));
+  }
+
+  return rightMenu;
 }
